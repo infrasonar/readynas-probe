d46e8f7ea20e5998ee4db9c96109b3d22a550bfc
Koos85 <koosjoosten@gmail.com>
2025-11-14 14:12:28 +0100

    libprobe v2

--- lib/check/disk.py
+++ lib/check/disk.py
@@ -1,5 +1,6 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from libprobe.asset import Asset
+from libprobe.check import Check
 from ..snmpclient import get_snmp_client
 from ..snmpquery import snmpquery
 
@@ -8,13 +9,14 @@ QUERIES = (
 )
 
 
-async def check_disk(
-        asset: Asset,
-        asset_config: dict,
-        check_config: dict):
+class CheckDisk(Check):
+    key = 'disk'
 
-    snmp = get_snmp_client(asset, asset_config, check_config)
-    state = await snmpquery(snmp, QUERIES)
-    for disk in state.get('diskEntry', []):
-        disk['diskCapacity'] = int(disk['diskCapacity'])
-    return state
+    @staticmethod
+    async def run(asset: Asset, local_config: dict, config: dict) -> dict:
+
+        snmp = get_snmp_client(asset, local_config, config)
+        state = await snmpquery(snmp, QUERIES)
+        for disk in state.get('diskEntry', []):
+            disk['diskCapacity'] = int(disk['diskCapacity'])
+        return state
--- lib/check/volume.py
+++ lib/check/volume.py
@@ -1,5 +1,6 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from libprobe.asset import Asset
+from libprobe.check import Check
 from ..snmpclient import get_snmp_client
 from ..snmpquery import snmpquery
 
@@ -8,18 +9,19 @@ QUERIES = (
 )
 
 
-async def check_volume(
-        asset: Asset,
-        asset_config: dict,
-        check_config: dict):
+class CheckVolume(Check):
+    key = 'volume'
 
-    snmp = get_snmp_client(asset, asset_config, check_config)
-    state = await snmpquery(snmp, QUERIES)
-    for vol in state.get('volumeEntry', []):
-        free = vol['volumeFreeSpace']
-        total = vol['volumeSize']
-        vol['volumeUsedSpace'] = used = total - free
-        vol['volumeUsedPercentage'] = 100 * used / total if total else None
-        vol['volumeFreePercentage'] = 100 * free / total if total else None
+    @staticmethod
+    async def run(asset: Asset, local_config: dict, config: dict) -> dict:
 
-    return state
+        snmp = get_snmp_client(asset, local_config, config)
+        state = await snmpquery(snmp, QUERIES)
+        for vol in state.get('volumeEntry', []):
+            free = vol['volumeFreeSpace']
+            total = vol['volumeSize']
+            vol['volumeUsedSpace'] = used = total - free
+            vol['volumeUsedPercentage'] = 100 * used / total if total else None
+            vol['volumeFreePercentage'] = 100 * free / total if total else None
+
+        return state
